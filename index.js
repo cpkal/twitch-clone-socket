9bb6b14f4b653ec777875c783d62dd599fe579fa
cpkal <haikal.putra.gustiansyah@upi.edu>
2024-08-31 21:01:22 +0700

    Add more streamer info, fix chat, add viewercount and streamelapsed

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const ws = require('ws');
-const wss = new ws.Server({ port: 8080 });
+const wss = new ws.Server({ port: 8081 });
 
 const streamers = {};
 
+const viewersOnStream = {};
+
 wss.on('connection', function connection(ws) {
+
+
   ws.on('message', function incoming(message) {
     const data = JSON.parse(message);
-    if (data.type === 'streamer') {
+    
+    if(data.type === 'join') {
+      viewersOnStream[data.streamer] = viewersOnStream[data.streamer] || [];
+      viewersOnStream[data.streamer].push(ws);
+
+      //viewer count
+      const viewerCount = viewersOnStream[data.streamer].length;
+
+      //send to all viewers
+      viewersOnStream[data.streamer].forEach((viewer) => {
+        viewer.send(JSON.stringify({ type: 'viewerCount', viewerCount }));
+      });
+
+    } else if (data.type === 'streamer') {
       streamers[data.id] = ws;
     } else if (data.type === 'viewer') {
       const streamer = streamers[data.id];
@@ -15,19 +32,19 @@ wss.on('connection', function connection(ws) {
         streamer.send(JSON.stringify({ type: 'viewer', id: data.id }));
       }
     } else if(data.type === 'chat') {
-      console.log('chat', data);
-      //send to all viewers
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === ws.OPEN) {
-          client.send(JSON.stringify({ type: 'chat', chat: data.chat }));
-        }
+      console.log('chat', data)
+
+      viewersOnStream[data.streamer].forEach((viewer) => {
+        console.log(data.chat);
+        viewer.send(JSON.stringify({ type: 'chat', chat: data.chat }));
       });
     }
-  });
-    
+  });    
   
   ws.on('close', function close() {
-    
+    for (const streamer in viewersOnStream) {
+      viewersOnStream[streamer] = viewersOnStream[streamer].filter((viewer) => viewer !== ws);
+    }
   });
 
   }
